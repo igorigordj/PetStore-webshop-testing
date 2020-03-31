2e3ced6ef5087218d7b81e6b9b6cac6b6fc63ac3
igorigordj <egor.gile@gmail.com>
2020-03-31 18:57:35 +0200

    Registration page and Registration page test

--- src/tests/RegistrationTest.java
+++ src/tests/RegistrationTest.java
@@ -9,36 +9,36 @@ public class RegistrationTest extends TestTemplateClass {
 
 	SoftAssert sa = new SoftAssert();
 	
-//	@Test
+	@Test
 	// Regular registration test (Expected: successful registration)
-//	public void registrationTest() throws IOException, InterruptedException {
-//		this.driver.navigate().to(this.locators.getProperty("registrationPage"));
-//		
-//		this.driver.manage().getCookies().clear();
-//
-//		RegistrationPage rp = new RegistrationPage (driver, selectors, locators, waiter);
-//		rp.completeRegistrationForm();
-//
-//		sa.assertTrue(rp.validateRegistration());
-//		sa.assertAll();
-//	}
+	public void registrationTest() throws IOException, InterruptedException {
+		this.driver.navigate().to(this.locators.getProperty("registrationPage"));
+		
+		this.driver.manage().getCookies().clear();
 
-//	@Test
+		RegistrationPage rp = new RegistrationPage (driver, selectors, locators, waiter);
+		rp.completeRegistrationForm();
+
+		sa.assertTrue(rp.validateRegistration());
+		sa.assertAll();
+	}
+
+	@Test
 	//Registration test without User name(Expected: registration fail)
-//	public void registrationTestWithoutUserName() throws IOException, InterruptedException {
-//		this.driver.navigate().to(this.locators.getProperty("registrationPage"));
-//		
-//		this.driver.manage().getCookies().clear();
-//
-//		
-//		SoftAssert sa = new SoftAssert();
-//
-//		RegistrationPage rp = new RegistrationPage (driver, selectors, locators, waiter);
-//		rp.completeRegistrationFormWithoutUserName();
-//
-//		sa.assertTrue(rp.validateRegistration());
-//		sa.assertAll();
-//	}
+	public void registrationTestWithoutUserName() throws IOException, InterruptedException {
+		this.driver.navigate().to(this.locators.getProperty("registrationPage"));
+		
+		this.driver.manage().getCookies().clear();
+
+		
+		SoftAssert sa = new SoftAssert();
+
+		RegistrationPage rp = new RegistrationPage (driver, selectors, locators, waiter);
+		rp.completeRegistrationFormWithoutUserName();
+
+		sa.assertTrue(rp.validateRegistration());
+		sa.assertAll();
+	}
 	
 	@Test
 	// Registration test only with User name and password, without aditional data(Expected: registration fail)
