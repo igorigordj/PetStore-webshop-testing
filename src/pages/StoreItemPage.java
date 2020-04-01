00e7df5e3c91c62fbe27686885e5e1bea17f7037
igorigordj <egor.gile@gmail.com>
2020-04-01 19:57:12 +0200

    StoreItemPage

--- src/pages/StoreItemPage.java
+++ src/pages/StoreItemPage.java
@@ -1,5 +1,25 @@
 package pages;
 
-public class StoreItemPage {
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class StoreItemPage extends AttributesClass{
+
+	public StoreItemPage(WebDriver driver, Properties selectors, Properties locators, WebDriverWait waiter) {
+		super(driver, selectors, locators, waiter);
+		
+	}
+	
+	public WebElement getAddToCartBtn() {
+		return this.driver.findElement(By.xpath(this.selectors.getProperty("addToCart")));
+	}
+	
+	public void addToCart() {
+		this.getAddToCartBtn().click();
+	}
 
 }
